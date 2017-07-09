948788b61f4b76f3e56e84a85a19aa3abcc953cc
lmdestiny <1453926032@qq.com>
2017-07-09 08:51:33 +0800

    push

--- SpringSecurity2/src/main/java/com/lmdestiny/security/security/repository/SysRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lmdestiny.security.security.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lmdestiny.security.domain.SysRole;
-
-public interface SysRoleRepository extends JpaRepository<SysRole, Integer> {
-
-}
